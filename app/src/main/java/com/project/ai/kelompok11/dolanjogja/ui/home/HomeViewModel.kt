a691ce158330fb756d82a67e9ab2ecedecd99e08
FAISALAKBARr <125114955+FAISALAKBARr@users.noreply.github.com>
2024-06-05 11:32:25 +0700

    Initial commit

---
+++ app/src/main/java/com/project/ai/kelompok11/dolanjogja/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.project.ai.kelompok11.dolanjogja.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
