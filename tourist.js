1d4a9a1656c0d198b34a452a59a8437013473b79
franklynical <jassonskywalker@gmail.com>
2024-06-26 20:42:11 +0700

    implementing oop?

---
+++ tourist.js
@@ -0,0 +1,90 @@
+export var tempatWisata = [
+  { name: "Kridosono Stadium", coords: [-7.782903, 110.375475] },
+  {
+    name: "Keraton Yogyakarta",
+    coords: [-7.806721, 110.364839],
+  },
+  { name: "Taman Sari", coords: [-7.81058, 110.35841] },
+  {
+    name: "Museum Benteng Vredeburg",
+    coords: [-7.80054, 110.36709],
+  },
+  { name: "Malioboro", coords: [-7.792354, 110.365785] },
+  {
+    name: "Taman Pintar",
+    coords: [-7.801438, 110.366778],
+  },
+  {
+    name: "Museum Sonobudoyo",
+    coords: [-7.801822, 110.363849],
+  },
+  { name: "Candi Prambanan", coords: [-7.75202, 110.4928] },
+  {
+    name: "Alun-Alun Kidul",
+    coords: [-7.813336, 110.360953],
+  },
+  {
+    name: "Pasar Beringharjo Beringharjo Market",
+    coords: [-7.798888, 110.367335],
+  },
+  {
+    name: "Jogja National Museum ",
+    coords: [-7.803971, 110.360091],
+  },
+  {
+    name: "De Mata Trick Eye Museum",
+    coords: [-7.810152, 110.37655],
+  },
+  {
+    name: "Situs Warungboto",
+    coords: [-7.802642, 110.388428],
+  },
+  { name: "Tugu Jogja", coords: [-7.782885, 110.367075] },
+  { name: "Stasiun Jogja Yogyakarta", coords: [-7.789228, 110.363091] },
+  {
+    name: "Bandara Adisutjipto",
+    coords: [-7.788965, 110.432026],
+  },
+  { name: "Ambarukmo Mall", coords: [-7.782716, 110.408844] },
+  {
+    name: "Jogja Night Tour",
+    coords: [-7.797068, 110.370529],
+  },
+  {
+    name: "Kebun Binatang Gembira Loka",
+    coords: [-7.792917, 110.397116],
+  },
+  { name: "Pakuwon Mall", coords: [-7.760072, 110.374233] },
+  {
+    name: "Jogja Exotarium Mini Zoo",
+    coords: [-7.75217, 110.314962],
+  },
+  { name: "Sleman City Hall", coords: [-7.73373, 110.360134] },
+  {
+    name: "Museum Pusat TNI AU Dirgantara",
+    coords: [-7.787945, 110.432526],
+  },
+  {
+    name: "Mrican Boat and Fishing Spot",
+    coords: [-7.779504, 110.417744],
+  },
+  {
+    name: "Sindu Kusuma Edupark",
+    coords: [-7.761553, 110.350289],
+  },
+  { name: "Jogja City Mall", coords: [-7.752974, 110.366159] },
+  { name: "Galleria Mall", coords: [-7.782399, 110.38075] },
+  {
+    name: "Museum Omah Jayeng",
+    coords: [-7.802362, 110.359275],
+  },
+  { name: "Museum Sandi", coords: [-7.782509, 110.375173] },
+  { name: "Museum Affandi", coords: [-7.782923, 110.398239] },
+  {
+    name: "Museum Sejarah Jawa",
+    coords: [-7.834846, 110.368321],
+  },
+  { name: "Stasiun Lempuyangan", coords: [-7.789787, 110.377669] },
+  { name: "Stasiun Maguwo", coords: [-7.789865, 110.436056] },
+  { name: "Senyum Tamanan", coords: [-7.831691, 110.366522] },
+];
